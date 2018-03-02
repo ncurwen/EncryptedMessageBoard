de448a8916204fd693e2f07a37eb2ccb70ba2e49
Nick Curwen <the.only.nick.curwen@gmail.com>
2018-03-01 22:34:08 -0500

    Complete provided ASP.NET C# web API tutorial with slight modifications.

---
+++ EncryptedMessageBoard/Models/EncryptedMessageContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EncryptedMessageBoard.Models
+{
+    public class EncryptedMessageContext : DbContext
+    {
+        public EncryptedMessageContext(DbContextOptions<EncryptedMessageContext> options) : base(options) { }
+
+        public DbSet<EncryptedMessage> EncryptedMessages { get; set; }
+    }
+}
